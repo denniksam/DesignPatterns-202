1e68d8d9204456a444a93bf204c864b5dd1be06c
Denys Samoilenko <denniksam@gmail.com>
2022-05-16 13:31:31 +0300

    State

---
+++ DesignPatterns/BehavioralPatterns/State.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.BehavioralPatterns
+{
+    internal class StateDemo
+    {
+        public void Show()
+        {
+            new Game().Update();
+        }
+    }
+
+    class Game
+    {
+        private IGameState state;
+        public IGameState State                      // Альтернатива:
+        {                                            // IGameState GetState() {
+            get => state;                            //   return state;
+            set                                      // }
+            {                                        // void SetState(IGameState newState) {
+                state = value;                       //   state = newState;
+                // Изменение вызывает "перерисовку"  //   Update();
+                Update();                            // }
+            }                                        // 
+        }                                            // 
+        public Game()
+        {
+            state = new MenuState(this);
+        }
+        public void Update()
+        {
+            state.Update();
+        }
+
+        // -------------- вариант б) ----------------
+        private List<IGameState> states = new List<IGameState>();
+
+        public void Play() 
+        {
+            // проверяем, есть ли уже созданный PlayState в предыдущих состояниях
+            foreach(var state in states)
+            {
+                if(state is PlayState)
+                {
+                    State = state;
+                    return;
+                }
+            }
+            // сюда мы попадаем если ни один return не сработал, то есть нет такого состояния
+            var newState = new PlayState(this);
+            states.Add(newState);
+            State = newState;
+        }
+        public void Pause() { State = new PauseState(this); }
+        public void Menu() { State = new MenuState(this); }
+
+    }
+
+    interface IGameState
+    {
+        void Update();
+    }
+
+    class MenuState : IGameState
+    {
+        private readonly Game game;
+        public MenuState(Game game)
+        {
+            this.game = game;
+        }
+        public void Update()
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("A key: Play");
+            Console.ReadKey(true);
+            // Переводим контекст (game) в новое состояние
+            // game.State = new PlayState(game);  // вариант а)
+            game.Play();  // вариант б)
+        }
+    }
+
+    class PlayState : IGameState
+    {
+        private readonly Game game;
+        public PlayState(Game game)
+        {
+            this.game = game;
+        }
+        public void Update()
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.WriteLine("Playing....");
+                key = Console.ReadKey(true);
+                if(key.Key == ConsoleKey.P)
+                {
+                    // game.State = new PauseState(game);  // вариант а)
+                    game.Pause();  // вариант б)
+                    return;
+                }
+            } while (key.Key != ConsoleKey.Escape);
+            
+        }
+    }
+
+    class PauseState : IGameState
+    {
+        private readonly Game game;
+        public PauseState(Game game)
+        {
+            this.game = game;
+        }
+        public void Update()
+        {
+            Console.WriteLine("Paused....");
+            Console.WriteLine(" Press a key");
+            Console.ReadKey(true);
+            // game.State = new PlayState(game);  // вариант а)
+            game.Play();  // вариант б)
+        }
+    }
+
+}
+/* Cостояние (State)
+Шаблон "Состояние" используется для реализациии приложений, имеющих разные
+"режимы" работы - состояния.
+Например, игра           // приложение подразумевает использование управления - 
+ - Состояние "Меню"      //  кнопок, манипуляторов. В режиме меню это управление
+ - Состояние "Игра"      //  выбирает пункты, а в "игре" - управляет игрой
+
+Без паттерна - 
+switch(GameMode){         // Без шаблона -- проблема дублирования кода
+    case Mode.Menu: ...   //  ? получения сигнала управления (KeyUp) - общее
+    case Mode.Play: ...   //  ? реакция на него различная
+    case Mode.Loading:    //  а в этом состоянии управление не нужно, то есть перенос
+}                         //   управление в общую область - нецелесообразно
+
+С паттерном - создаются классы, реализующие поведения в разных состояниях
+ один из них является "текущим"
+class Game {
+  GameState state;
+  ... state = new MenuState(); ...
+}
+class MenuState, PlayState, LoadingState : GameState
+LoadingState { OnFinish() { Game.state = new PlayState() | Game.Play()  } }
+PlayState {    OnEscape() { Game.state = new MenuState() | Game.Menu()  } }
+
+а) подход Game.state = new MenuState()
+ + хорошая читаемость
+ - сложно перевести на повторное использование объектов (Singleton), т.к.
+    в конструктор передается ссылка на контекст. Можно отделить SetContext(game), но
+    тогда надо вызывать методы "дублетом":
+     MenuState.SetContext(game);
+     Game.State = MenuState.GetInstance(); 
+    можно передать ссылку в GetInstance(game); но придется принимать решения что
+     делать если в новом вызове game поменяется
+
+б) подход Game.Menu()
+ + переносит логику создания / повторного использования объектов-состояний
+    в контекст -- не объекты помнят о своем предыдущем создании, 
+    а контекст помнит о предыдущих состояниях
+
+Д.З. Реализовать методы Game::Pause() и Game::Menu() для варианта б) с проверкой
+      на существование ранее созданного объекта-состояния
+     Доработать UML-диаграмму отобразив 
+      поля game в Состояниях и 
+      методы Play, Menu, Pause в Game
+      взаимосвязи с Show, Program
+*/
---
+++ DesignPatterns/StructuralPatterns/Composite.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StructuralPatterns
+{
+    internal class CompositeDemo
+    {
+        public void Show()
+        {
+            Firm firm = new Firm();
+            firm.Members.Add(new FinDirector());
+            firm.Members.Add(new ExeDirector());
+            firm.Members.Add(new LabourUnion());
+            firm.Members.Add(new DivisionsDelegate());
+
+            firm.MakeResolution("Bake a cookie");
+        }
+    }
+
+    interface IMember
+    {
+        void TakeResolution(String resolution);
+    }
+
+    class FinDirector : IMember
+    {
+        public void TakeResolution(String resolution)
+        {
+            Console.WriteLine($"FinDirector take '{resolution}' ");
+        }
+    }
+    class ExeDirector : IMember
+    {
+        public void TakeResolution(String resolution)
+        {
+            Console.WriteLine($"ExeDirector take '{resolution}' ");
+        }
+    }
+
+    class Firm
+    {
+        public List<IMember> Members { get; set; }
+        public Firm()
+        {
+            Members = new List<IMember>();
+        }
+        public void MakeResolution(String resolution)
+        {
+            Console.WriteLine($"Firm make resolution '{resolution}'");
+            foreach (IMember member in Members)
+            {
+                if(member is CompositeMember)
+                {
+                    Console.Write("[]");  
+                    // Д.З. Компоновщик:
+                    //  Добавить в признак комплексного участника количество
+                    //  конкретных участников в нем: []  -->  [3]
+                    // Закончить UML диаграмму
+                }
+                member.TakeResolution(resolution);
+            }
+        }
+    }
+    //------------------------- Complex member ---------------------
+
+    abstract class CompositeMember : IMember
+    {
+        public List<IMember> Members { get; set; }
+
+        public abstract void TakeResolution(string resolution);
+    }
+    class BakersDelegate : IMember
+    {
+        public void TakeResolution(String resolution)
+        {
+            Console.WriteLine($"BakersDelegate take '{resolution}' ");
+        }
+    }
+    class CleaningsDelegate : IMember
+    {
+        public void TakeResolution(String resolution)
+        {
+            Console.WriteLine($"CleaningsDelegate take '{resolution}' ");
+        }
+    }
+    class LoadersDelegate : IMember
+    {
+        public void TakeResolution(String resolution)
+        {
+            Console.WriteLine($"LoadersDelegate take '{resolution}' ");
+        }
+    }
+    class LabourUnion : CompositeMember
+    {
+        public LabourUnion()
+        {
+            Members = new List<IMember>() {
+                new BakersDelegate(),
+                new CleaningsDelegate(),
+                new LoadersDelegate()
+            };
+        }
+        override public void TakeResolution(String resolution)
+        {
+            Console.WriteLine($"LabourUnion take resolution '{resolution}'");
+            foreach (IMember member in Members)
+            {
+                Console.Write(" ✅ "); 
+                member.TakeResolution(resolution);
+            }
+        }
+    }
+
+    class MykolaivDelegate : IMember
+    {
+        public void TakeResolution(String resolution)
+        {
+            Console.WriteLine($"MykolaivDelegate take '{resolution}' ");
+        }
+    }
+    class KhersonDelegate : IMember
+    {
+        public void TakeResolution(String resolution)
+        {
+            Console.WriteLine($"KhersonDelegate take '{resolution}' ");
+        }
+    }
+    
+    class IzmailDelegate : IMember
+    {
+        public void TakeResolution(String resolution)
+        {
+            Console.WriteLine($"IzmailDelegate take '{resolution}' ");
+        }
+    }
+    class VilkovoDelegate : IMember
+    {
+        public void TakeResolution(String resolution)
+        {
+            Console.WriteLine($"VilkovoDelegate take '{resolution}' ");
+        }
+    }
+    class OdessaDelegate : CompositeMember
+    {
+        public OdessaDelegate()
+        {
+            Members = new List<IMember>()
+            {
+                new IzmailDelegate(),
+                new VilkovoDelegate()
+            };
+        }
+        override public void TakeResolution(String resolution)
+        {
+            Console.WriteLine($"OdessaDelegate(s) take resolution '{resolution}'");
+            foreach (IMember member in Members)
+            {
+                Console.Write("    ✅ ");
+                member.TakeResolution(resolution);
+            }
+        }
+    }
+    
+    class DivisionsDelegate : CompositeMember
+    {
+        public DivisionsDelegate()
+        {
+            Members = new List<IMember>() {
+                new KhersonDelegate(),
+                new MykolaivDelegate(),
+                new OdessaDelegate()
+            };
+        }
+        override public void TakeResolution(String resolution)
+        {
+            Console.WriteLine($"DivisionsDelegate(s) take resolution '{resolution}'");
+            foreach (IMember member in Members)
+            {
+                Console.Write(" ✅ ");
+                member.TakeResolution(resolution);
+            }
+        }
+    }
+}
+/*
+ Компоновщик (Composite)
+Объединение составных объектов наравне с одиночными.
+Возможность в качестве элемента использовать комплексный набор.
+
+Похож на Декоратор, но Декоратор -- одномерный, Компоновщик -- древовидный
+Также похож на Декоратор тем, что не рекомендуется как фундамент проекта,
+ а приходит на помощь при необходимости расширять проект
+
+Идея:
+ Есть совет директоров, в нем участвуют представители
+ В какой-то момент открывается филиал и появляется комплексный представитель
+  с одной стороны, у него один голос (один участник)
+  с другой стороны, реальных представителей несколько
+
+Решение:
+ Внедрение в систему комплексного участника, который
+  реализует интерфейс одиночного участника
+  имеет "внутреннюю" возможность распределять (дублировать) задачи
+   для всех своих участников
+ */
